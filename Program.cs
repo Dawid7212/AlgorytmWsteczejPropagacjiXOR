c5ab8a78ce44f119a68c4d6e6efc667e5fdaee91
Dawid7212 <dawidMoeck002@gmail.com>
2025-05-14 15:00:53 +0200

    zmiana przechowywania wag na tablice 3d, tak aby dało się z niej wyciagac wagi o które mi chodzi

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace AlgorytmWsteczejPropagacjiXOR
 
         private static Random rand = new Random();
 
-        public static double[] PropagacjaWsteczna(double[] wejscie, double[] OczekiwaneWyjscia, double[] wagi, int[] struktura, double parametrUczenia)
+        public static double[][][] PropagacjaWsteczna(double[] wejscie, double[] OczekiwaneWyjscia, double[][][] wagi, int[] struktura, double parametrUczenia)
         {
 
             double[][] WynikiSieci = SiecNeuronowa(wejscie, wagi, struktura);
@@ -33,33 +33,40 @@ namespace AlgorytmWsteczejPropagacjiXOR
                     double SumaBledow = 0;
                     for (int j = 0; j < struktura[nrWarstwy+1]; j++)//iteracja po liczbie neuronów w warstwie kolejnej -> każdy neuron z warstwy obeznej ma połączenie ze wszystkimi neuronami warstwy nastepnej
                     {
-                        SumaBledow += BledyNeuronow[nrWarstwy + 1][j] * wagi[];
+                        SumaBledow += BledyNeuronow[nrWarstwy + 1][j] * wagi[nrWarstwy][j][i+1];//tylko dla połączeń miedzy neuronami, błąd neuronu kolejnego* waga łącząca te nurony
                     }
-                    BledyNeuronow[nrWarstwy][i] = SumaBledow * (B * WynikiSieci[nrWarstwy][i]*(1- WynikiSieci[nrWarstwy][i]));
+                    BledyNeuronow[nrWarstwy-1][i] = SumaBledow * (B * WynikiSieci[nrWarstwy][i]*(1- WynikiSieci[nrWarstwy][i]));
                 }
 
             }
 
 
-            return wynik;
+            return wagi;
         }
 
 
 
 
-        public static double[] WylosujWagi(int[] strukturaSieci)
+        public static double[][][] WylosujWagi(int[] strukturaSieci)
         {
             int liczbaWag = 0;
+            double[][][] wagi = new double[strukturaSieci.Length - 1][][];
             for (int i = 0; i < strukturaSieci.Length - 1; i++)
-                liczbaWag += (strukturaSieci[i] + 1) * strukturaSieci[i + 1];// bo np.: (liczbaneuronowPoprzedniejWarstwy +1 wagaDodatkowaKażdegoNeuronu)*iloscneuronowWKolejnejWarstwie
-
-            double[] wagi = new double[liczbaWag];
-            for (int i = 0; i < wagi.Length; i++)
-                wagi[i] = rand.NextDouble() * 2 - 1;
+            {
+                wagi[i] = new double[strukturaSieci[i + 1]][];//+1 bo liczba wag zalezna od nastepnej warstwy
+                for(int j =0; j < strukturaSieci[i + 1]; j++)
+                {
+                    for(int ktoraWaga = 0; ktoraWaga < strukturaSieci[i] + 1; ktoraWaga++)
+                    {
+                        wagi[i][j][ktoraWaga] = (rand.NextDouble()*2)-1;
+                    }
+                }
+            }
+                
 
             return wagi;
         }
-        public static double[][] SiecNeuronowa(double[] wejscie, double[] Wagi, int[] struktura)
+        public static double[][] SiecNeuronowa(double[] wejscie, double[][][] Wagi, int[] struktura)
         {
             int lWarstw = struktura.Length;
             double[][] wyjscie = new double[lWarstw][]; // to będzie zlepek wszystkich wyników neuronów podzielonych na warstwy
@@ -70,7 +77,7 @@ namespace AlgorytmWsteczejPropagacjiXOR
                 wyjscie[0][i] = wejscie[i];
             }
 
-            int indeksWagi = 0;
+
             for (int i = 1; i < lWarstw; i++)
             {
                 int LiczbaNeuronowWarstwy = struktura[i];
@@ -79,13 +86,7 @@ namespace AlgorytmWsteczejPropagacjiXOR
 
                 for (int n = 0; n < LiczbaNeuronowWarstwy; n++)
                 {
-                    int k = LiczbaNuronowPoprzedniejWarstwy + 1;
-                    double[] wagiNeuronu = new double[k];
-                    for (int j = 0; j < k; j++)
-                    {
-                        wagiNeuronu[j] = Wagi[indeksWagi];
-                        indeksWagi++;
-                    }
+                    double[] wagiNeuronu = Wagi[i - 1][n];
 
                     double z = Neuron(wagiNeuronu, wyjscie[i - 1]);
                     wyjscie[i][n] = FAktywacji(z);
@@ -122,7 +123,14 @@ namespace AlgorytmWsteczejPropagacjiXOR
                 WarstwyIneurny[i] = liczbaNeuWarstwUkrytej;
             }
             WarstwyIneurny[liczbaWarstw - 1] = liczbaWyjsc;
-            double[] wagi = new double[] { 0.3, 0.1, 0.2, 0.6, 0.4, 0.5, 0.9, 0.7, -0.8 };
+            //double[] wagi = new double[] { 0.3, 0.1, 0.2, 0.6, 0.4, 0.5, 0.9, 0.7, -0.8 };
+            double[][][] wagi = new double[2][][];
+            wagi[0] = new double[2][];
+            wagi[1] = new double[2][];
+            wagi[0][0] = new double[] { 0.3, 0.1, 0.2  };
+            wagi[0][1] = new double[] { 0.6, 0.4, 0.5 };
+            wagi[1][1] = new double[] { 0.9, 0.7, -0.8 };
+           
             int[] Struktura = new int[] { 2, 2, 1 };
             double[] wejscie = new double[] { 1, 0 };
             double[][] WynikSieci = SiecNeuronowa(wejscie, wagi, Struktura);
